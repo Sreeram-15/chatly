6b04b927a2c3050d2664cc490dbc2011ef0a91dc
Sreeram <bingisreeram99@gmail.com>
2025-10-15 23:43:43 +0530

    signup code refactor commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDb=async ()=>{
     try{
+        const {MONGO_URL}=process.env;
+        if(!MONGO_URL){
+            throw new Error("MONGO_URI is not set");
+        }
         const conn=await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDb connected: ",conn.connection.host);
 
